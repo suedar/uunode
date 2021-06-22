3883fc3a121f7e3e82dccf63e4cb51900f160318
suedar <690372513@qq.com>
2021-06-22 15:34:45 +0800

    init project

--- index.js
+++ index.js
@@ -4,11 +4,15 @@ const path = require('path');
 const run = require('./loader.js');
 const chalk = require("chalk");
 
+const main = () => {
+  const argv = require('yargs-parser')(process.argv.slice(2))
 
-const argv = require('yargs-parser')(process.argv.slice(2))
-if (!Array.isArray(argv._) || argv._.length === 0) {
-  console.log(chalk.red("Error Occurred: Please Input FilePath"));
-  return;
-}
+  if (!Array.isArray(argv._) || argv._.length === 0) {
+    console.log(chalk.red("Error Occurred: Please Input FilePath"));
+    return;
+  }
 
-run(path.resolve(__dirname, argv._[0]))
+  run(path.resolve(__dirname, argv._[0]), argv);
+};
+
+main();
--- loader.js
+++ loader.js
@@ -1,55 +1,73 @@
-const fs = require('fs');
+const fs = require("fs");
 const util = require('util');
-const rmdir = util.promisify(fs.rmdir);
+const path = require('path');
+const { exec } = require("child_process");
+
+const rmdir = util.promisify(fs.rmdirSync);
+const menuName = '.uunode';
 
 const deleteMenu = async () => {
-  await rmdir('./.unode', { recursive: true });
-};
+  rmdir(path.resolve(__dirname, menuName), {recursive: true});
+}
 
-const createFile = (filePath) => {
-  let data = fs.readFileSync(filePath, 'utf-8');
-
-  const importReg = /^import.*'|"$/gm;
-
-  data = data.replace(importReg, (str) => {
+const transJsToMjs = (data, filePaths, filePath) => {
+  const importReg = /^import.*'|"$/mg;
+  const newData = data.replace(importReg, (str) => {
     if (!/from/.test(str)) {
       // TODO: 转为采用 deno 执行
-      // throw new Error(`
-      //   Unable to resolve module.
-      //   Please check the code at ${filePath}
-      // `);
+      throw new Error(`暂未支持模块直接引入`);
     }
-    const strArr = str.split(/import|from/);
-    const newArr = [
-      'const ',
-      strArr[1].trim(),
-      ' = require(',
-      strArr[2].trim(),
-      ')',
-      ';',
-    ];
-    return newArr.join('');
-  });
+    const strArr = str.split(/'|"/);
+    const module = strArr[1];
 
-  console.log(data);
-};
+    if (/js|mjs/.test(module)) {
+      const pathArr = filePath.split('/');
+      pathArr.pop();
+      pathArr.push(module);
+      filePaths.push(pathArr.join('/'));
+    }
+    if (/js/.test(module)) {
+      return [strArr[0], `"${module.slice(0, -2)}mjs"`, strArr[2]].join('');
+    }
+    return str;
+  });
+  return newData;
+}
+
+const createFile = (filePath) => {
+  const filePaths = [];
+  let data = fs.readFileSync(filePath, "utf-8");
+  data = transJsToMjs(data, filePaths, filePath);
+
+  const pureFilePath = filePath.split("/").reverse()[0];
+  let actualPath = pureFilePath;
+
+  if (/js/.test(pureFilePath)) {
+    actualPath = pureFilePath.slice(0, -2) + 'mjs';
+  }
+  fs.mkdirSync(menuName, {recursive: true});
+  fs.writeFileSync(path.resolve(__dirname, menuName, actualPath), data);
+
+  while (filePaths.length > 0) {
+    const curPath = filePaths.shift();
+    createFile(curPath);
+  }
+
+  return actualPath;
+}
 
 /**
  * main file
  * @param {string} filePath
  */
-const run = async (filePath) => {
-  if (filePath === undefined) {
-    throw new Error(
-      `Unable to resolve filePath, please input correct filePath.`
-    );
-  }
-
+const run = async (filePath, argv) => {
   deleteMenu();
-  createFile(filePath);
+  const renderPath = createFile(filePath, menuName);
 
-  // console.log(data);
-  // console.log("READ FILE SYNC END");
+  exec(`node ${path.resolve(__dirname, menuName, renderPath)}`, (err, stdout, stderr) => {
+    !argv.s && console.log(stdout);
+    !argv.k && deleteMenu();
+  });
 };
 
 module.exports = run;
