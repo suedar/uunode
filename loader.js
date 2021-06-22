85e087a0144ff51a63d937aaaf3fe53942fccbc4
suedar <690372513@qq.com>
2021-06-22 18:32:51 +0800

    😳finish

--- loader.js
+++ loader.js
@@ -21,8 +21,6 @@ const handleFilePath = (filePath) => {
   }
   // filePath = removeCurrentLink(filePath);
 
-  // 如果出现 .. 则放置在根目录下
-  // 如果不是就根据目录相应放置
   return filePath.replace(/.+\//, "");
 }
 
@@ -46,7 +44,7 @@ const transJsToMjs = (data, filePaths, filePath) => {
     }
 
     if (/js/.test(module)) {
-      return [strArr[0], `"${handleFilePath(module.slice(0, -2) + 'mjs')}"`, strArr[2]].join('');
+      return [strArr[0], `"./${handleFilePath(module.slice(0, -2) + 'mjs')}"`, strArr[2]].join('');
     }
     return str;
   });
@@ -60,29 +58,9 @@ const createFile = (filePath) => {
   const filePathArr = filePath.split("/").reverse();
   const filePathArrLastPath = filePathArr.shift();
   const actualPath = /js/.test(filePathArrLastPath) ? filePathArrLastPath.slice(0, -2) + 'mjs' : filePathArrLastPath;
-  filePathArr.unshift(actualPath);
 
   fs.mkdirSync(menuName, { recursive: true });
-  const curFilePath = path.resolve(rootPath, menuName, filePathArr.reverse().join('/').split(rootPath)[1].slice(1));
-  console.log(curFilePath, actualPath)
-  // 创建 menu
-  const rootReg = new RegExp(rootPath)
-  const curFilePathArr = curFilePath.replace(rootReg, '').split('/');
-  curFilePathArr.pop();
-  const curMenu = [];
-  while (curFilePathArr.length > 0) {
-    const curMenuPath = curFilePathArr.shift();
-    if (!curMenuPath) {
-      continue;
-    }
-    curMenu.push(curMenuPath);
-    const menu = curMenu.join('/');
-    if (!fs.existsSync(menu)) {
-      fs.mkdirSync(menu, { recursive: true });
-    }
-  }
-
-  fs.writeFileSync(curFilePath, data, { recursive: true });
+  fs.writeFileSync(path.resolve(rootPath, menuName, handleFilePath(actualPath)), data, { recursive: true });
 
   while (filePaths.length > 0) {
     const curPath = filePaths.shift();
